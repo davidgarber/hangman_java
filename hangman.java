ca72e2207dcc76cf8a501f1c42447290d8dceddf
David Garber <davidgarber@gmail.com>
2015-08-25 09:23:47 -0700

    Be able to make a guess of a letter and return hit or miss.

--- hangman.java
+++ hangman.java
@@ -3,5 +3,7 @@ public class Hangman {
   public static void main(String[] args) {
 
       Game game = new Game("treehouse");
+      Prompter prompter = new Prompter(game);
+      boolean isHit = prompter.promptForGuess();
   }
 }
